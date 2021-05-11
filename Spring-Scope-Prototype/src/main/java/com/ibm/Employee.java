bef920cb9053b8fa5521d182ea87f56bb399bdb4
mahi <mahi@mahi-pc>
2021-05-11 17:52:08 +0545

    Spring Scope-Prototype

---
+++ Spring-Scope-Prototype/src/main/java/com/ibm/Employee.java
@@ -0,0 +1,29 @@
+package com.ibm;
+
+public class Employee {
+	
+	private String fullName;
+	private String salary;
+	
+	public Employee() {
+		System.out.println("singleton Bean Instantiated," +"Create everytime new Object....!!");
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+
+}
